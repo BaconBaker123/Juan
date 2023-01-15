10d00d3555fa1c0d7d46ec2b05090c7aea268ea1
JihangLi1121 <ljh20021121@gmail.com>
2023-01-15 00:30:26 -0800

    adding stock_analysis

---
+++ src/data_analysis/stock_analysis.py
@@ -0,0 +1,34 @@
+# Raw Package
+import numpy as np
+import pandas as pd
+
+#Data Source
+import yfinance as yf
+
+google = yf.Ticker("T") 
+#shares out standing 
+print(google.shares) 
+print("--------------------------")
+print(google.get_shares_full())
+
+#earnings 
+print("--------------------------")
+print(google.earnings)
+print("--------------------------")
+print(google.quarterly_earnings)
+
+#dividends
+print("--------------------------")
+print(google.dividends)
+
+#income statement
+print("--------------------------")
+print(google.income_stmt)
+
+#balance sheet
+print("--------------------------")
+print(google.balance_sheet.to_string())
+
+#cash_flow
+print("--------------------------")
+print(google.cash_flow.to_string())
