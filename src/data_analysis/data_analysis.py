8049f0df8e894233d6f94610e25bb1134b57f890
JihangLi1121 <ljh20021121@gmail.com>
2023-01-14 21:45:22 -0800

    init

--- src/data_analysis/data_analysis.py
+++ src/data_analysis/data_analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #dataframe from excel
-dataframe = pd.read_excel(r'./../data_collection_API/RoboAdvisor 1.1 (reply).xls')
+dataframe = pd.read_excel(r'/data_collection_API/RoboAdvisor 1.1 (reply).xls')
 print(dataframe)
 
 #could also get form data from 'Juan/src/questionary_API/reply_content'
