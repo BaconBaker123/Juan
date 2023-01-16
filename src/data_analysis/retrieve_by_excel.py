d136f908ce39125a56f94d8bf09e091d26c2f277
JihangLi1121 <ljh20021121@gmail.com>
2023-01-15 21:27:23 -0800

    test run retrieve by excel

--- src/data_analysis/retrieve_by_excel.py
+++ src/data_analysis/retrieve_by_excel.py
@@ -13,6 +13,6 @@ gauth.LocalWebserverAuth()
 drive = GoogleDrive(gauth)
   
 # Initialize GoogleDriveFile instance with file id
-file_obj = drive.CreateFile({'id': '1a3h5esLr0i6EfDqwiR01fXx6I-iBBIxbdk66cJ8N8-U'})
+file_obj = drive.CreateFile({'id': '1o52jX9JPXlvzcAEF1S4nxwrDCzv9nVnbRkQZLYdEVm0'})
 file_obj.GetContentFile('RoboAdvisor 1.1 (Responses).xls',
          mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
